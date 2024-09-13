bc035bebcebcad8e73bdb5d386ca113a7687a5ec
Alex Feng <146572860+axfng@users.noreply.github.com>
2024-09-13 16:01:03 -0400

    API fetching working

--- AppTeamTechnical/ContentView.swift
+++ AppTeamTechnical/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var products: [Result]
+}
+
+struct Result: Codable, Identifiable {
+    var id: Int
+    var title: String
+    var description: String
+    var category: String
+    var price: Double
+    var images: [String]
+}
+
 struct ContentView: View {
+    @State private var products = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(products, id: \.id) {item in
+                Text(item.title)
+            }
+            .task {
+                await loadData()
+            }
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://dummyjson.com/products") else{
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                products = decodedResponse.products
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
