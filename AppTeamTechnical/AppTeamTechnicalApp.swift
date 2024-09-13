bfcc88e8d11f198e9b6e0773e5236c95542a175a
Alex Feng <146572860+axfng@users.noreply.github.com>
2024-09-12 22:15:47 -0400

    Initial Commit

---
+++ AppTeamTechnical/AppTeamTechnicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTeamTechnicalApp.swift
+//  AppTeamTechnical
+//
+//  Created by alfeng on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTeamTechnicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTeamTechnical/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTeamTechnical
+//
+//  Created by alfeng on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
